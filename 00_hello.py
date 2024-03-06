30170026f96e6e4b8cfee1e4e88f28adadf6cf63
JRichard-Prado <jrichip@hotmail.com>
2024-03-06 16:39:16 +0100

    correccion del text

--- 00_hello.py
+++ 00_hello.py
@@ -11,24 +11,25 @@ print(''' hola tres comillas''') #String en comillas triples
 print(type("hola type")) # devuelve el tipo de la variable
 print(type(True))
 print(type({1.2, 1.2, "sdf "}))
-"""Definicion de variables 
-las variables son de tipo dimanico cambian automaticamente de acuerdo al valor asignado
+##############Definicion de variables###############
+""" 
+Las variables son de tipo dimanico cambian automaticamente de acuerdo al valor asignado
 """
 semanas=4
 nom_mama= "Gaby"
 nom_papa= "Richard"
 texto=" y "
 peso=65.2
-# formateo de string 
+# Formateo de string 
 print("en {} semanas hay {} dias".format(semanas,7*semanas)  )
 print("en %d semanas hay %d dias" %(semanas,7*semanas))
 print(f"en {semanas} semanas hay {7*semanas} dias")
-#impresion sin salto de linea
+#Impresion sin salto de linea
 print(f"{nom_mama} pregunta 'como estas??? '",end='dentro de '+str(semanas)+" semanas....... ")
-print(f"{nom_papa} contesta \"muy bien gracias\"")# comillas dobles dentro de comillas dobles
+print(f"{nom_papa} contesta \"muy bien gracias\"")# comillas dobles dentro de comillas dobles con \
 print('un tipo pregunta "como estas?..."',end=" ")
 print('el otro contesta \' not bat\'')
-#impresion sin salto de linea concatenado con un texto
+#Impresion sin salto de linea concatenado con un texto
 print("'Hola'",end=texto)
 print("\"Adios\"")
 print(nom_mama,end=texto)
